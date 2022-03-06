5f2b1c077546b58f31a0f4cd3abf8156cbccc722
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-03-06 07:43:23 +0600

    Update

--- base/models.py
+++ base/models.py
@@ -6,4 +6,23 @@ from django.contrib.auth.models import AbstractUser
 
 # The target is to login with email rather than username
 class User(AbstractUser):
-    pass
+    # this pass gives the default user 
+    # fields in base app user
+    # pass
+
+    # it's going to be set to null otherwise it might be an issue
+    name = models.CharField(max_length=200, null=True)
+    #email_field unique set to true meaning
+    # emails can't be same 
+    email = models.EmailField(unique=True)
+    # we already have a model here so to make
+    # sure that user doesn't have to insert a 
+    # bio
+    bio = models.TextField(null=True)
+
+    # it's telling django the username field is
+    # going to be the email field
+    # after doing just make the migrations 
+    # and it should work
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
